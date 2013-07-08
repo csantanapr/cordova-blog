3ab0c53dbaeeafebc401aa839b86e4840040149c
Carlos Santana <csantana23@gmail.com>
2013-07-08 11:11:40 -0400

    Added rss feed to site

--- public/build.sh
+++ public/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-open "http://localhost:4000/"
-jekyll serve --baseurl ''
+jekyll build
+
 
---
+++ serve.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+echo "open your browser http://localhost:4000/"
+echo "Running jekyll serve --watch --baseurl '' "
+echo "******************************************************"
+jekyll serve --watch --baseurl ''
+
+
