0ce3ef2f844d7ac66534c87f853cffd85fe88a37
Carlos Santana <csantana23@gmail.com>
2013-07-10 02:36:14 -0400

    Sync to match svn repo

--- www/serve.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "open your browser http://localhost:4000/"
-echo "Running jekyll serve --watch --baseurl '' "
-echo "******************************************************"
-jekyll serve --watch --baseurl ''
-
-
